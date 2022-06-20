d313e943757196a115b64f80fdbb07596791d59b
Knienow <nienowkatia@gmail.com>
2022-06-20 08:19:55 -0300

    Classes Abstratas e Interfaces
    20/06/22

---
+++ ConceitoClassesAbstrataseInterfaces/app/src/main/java/nienow/katia/uno/conceitoclassesabstrataseinterfaces/formas/Retangulo.java
@@ -0,0 +1,22 @@
+package nienow.katia.uno.conceitoclassesabstrataseinterfaces.formas;
+
+import nienow.katia.uno.conceitoclassesabstrataseinterfaces.formas.Forma;
+
+public class Retangulo extends Forma {
+    public Retangulo(double altura, double largura) {
+        super(2); // uso obrigatório do construtor da superclasse
+        setMedida(0, altura); // ajuste da medida da altura
+        setMedida(1, largura); // ajuste da medida da largura
+    }
+
+    // implementação do método abstrato area()
+    @Override
+    public double area() {
+        return getMedida(0) * getMedida(1);
+    }
+
+    // implementação de operação específica extra
+    public double perimetro() {
+        return 2 * (getMedida(0) + getMedida(1));
+    }
+}
