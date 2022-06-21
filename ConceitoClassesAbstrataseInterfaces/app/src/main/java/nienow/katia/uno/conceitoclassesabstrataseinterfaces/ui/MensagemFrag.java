0de41aab72952b7c8790dca0d471fc8d50b1a722
Knienow <nienowkatia@gmail.com>
2022-06-21 11:26:07 -0300

    Aula 21/06
    Classes abstratas

--- ConceitoClassesAbstrataseInterfaces/app/src/main/java/nienow/katia/uno/conceitoclassesabstrataseinterfaces/ui/MensagemFrag.java
+++ ConceitoClassesAbstrataseInterfaces/app/src/main/java/nienow/katia/uno/conceitoclassesabstrataseinterfaces/ui/MensagemFrag.java
@@ -1,11 +1,15 @@
 package nienow.katia.uno.conceitoclassesabstrataseinterfaces.ui;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import nienow.katia.uno.conceitoclassesabstrataseinterfaces.R;
+
 
 public class MensagemFrag extends Fragment {
 
@@ -14,12 +18,14 @@ public class MensagemFrag extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View layoutFragment = inflater.inflate(R.layout.fragment_mensagem, container, false);
+
 
         return layoutFragment;
     }
