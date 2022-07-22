8595bc127cdcb131d83d792fc59c085f06633ef3
Knienow <nienowkatia@gmail.com>
2022-07-22 11:24:53 -0300

    Commit 22/07
    MOR integração com Java - criação de pacotes e classes.

---
+++ MORIntegraocomJava/app/src/main/java/nienow/katia/uno/morintegraocomjava/database_app/DatabaseApp.java
@@ -0,0 +1,42 @@
+package nienow.katia.uno.morintegraocomjava.database_app;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.concurrent.Executors;
+
+public abstract class DatabaseApp extends RoomDatabase {
+    //CREATE DATABASE db_app;
+    private static final String DB_NAME = "db_app.db";
+    private static volatile DatabaseApp sInstance;
+    private static final String TAG = "O banco foi criado com sucesso! Executar rotinas posteriores.";
+
+    public DatabaseApp() {
+    }
+
+    public static synchronized DatabaseApp getInstance(Context context){
+        return null;
+    }
+
+    private static DatabaseApp getConection(Context context){
+        return null;
+    }
+
+    static RoomDatabase.Callback initDB = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            Executors.newSingleThreadScheduledExecutor().execute(() -> {
+                //executar métodos e/ou rotinas logo após a criação do banco
+                Log.d(TAG, "O banco foi criado com sucesso! Executar rotinas posteriores.");
+            });
+        }
+        public void onOpen(SupportSQLiteDatabase db) {
+        }
+    };
+}
+
