d948a57d630b95c878975536a5476b30313f1ba4
Katia Nienow <95932159+Knienow@users.noreply.github.com>
2022-09-29 11:01:25 -0300

    aula 14-09-22

---
+++ aula-14-09-22/js/script.js
@@ -0,0 +1,20 @@
+alert("Alô mundo!");
+
+{
+document.write("<h1>Este é um cabeçalho</h1>");
+document.write("<p>Isto é um parágrafo.</p>");
+document.write("<p>Este é um outro parágrafo</p>");
+}
+
+//escreve um cabeçalho
+document.write("<h1>Este é um cabeçalhooooooooooooooooo</h1>");
+
+nome=prompt("Qual é o seu nome?");
+
+document.write("<h3>"+nome+", seja bem-vindo(a) à minha página</h3>");
+
+a=25;
+b=78;
+alert(a + "+" +  b + "=" + (a+b));
+
+document.write(a + "+" +  b + "=" + (a+b));
